e9f37fa6a12edc423bb167bf9eb70d64ca112b01
shanmugapriya-93 <priya@ecrio.com>
2025-04-16 08:10:59 +0000

    new pal_stub.c

--- kaios_rcs-main/lims/main/src/pal_stub.c
+++ kaios_rcs-main/lims/main/src/pal_stub.c
@@ -1,4 +1,4 @@
-// pal_stub.c (create this file manually)
+// pal_stub.c
 #include <stdio.h>
 
 typedef void* KPALHandle;
@@ -16,14 +16,13 @@ unsigned int pal_Deinit(KPALHandle palHandle) {
     return 0;
 }
 
-unsigned int pal_LogDeinit(KLogHandle* logHandle) {
-    printf("Stub: pal_LogDeinit()\n");
-    return 0;
+KLogHandle pal_LogInit(void* filename, int output, int mask, int level, int component, int type) {
+    printf("Stub: pal_LogInit()\n");
+    return (KLogHandle)0x3; // fake log handle
 }
 
-unsigned int pal_MutexCreate(KPALHandle palHandle, KMutexHandle* mutexHandle) {
-    printf("Stub: pal_MutexCreate()\n");
-    *mutexHandle = (void*)0x2;
+unsigned int pal_LogDeinit(KLogHandle* logHandle) {
+    printf("Stub: pal_LogDeinit()\n");
     return 0;
 }
 
@@ -32,7 +31,29 @@ unsigned int pal_SocketSetDeviceName(KPALHandle palHandle, const char* name) {
     return 0;
 }
 
-KLogHandle pal_LogInit(void* filename, int output, int mask, int level, int component, int type) {
-    printf("Stub: pal_LogInit()\n");
-    return (KLogHandle)0x3;
+unsigned int pal_MutexCreate(KPALHandle palHandle, KMutexHandle* mutexHandle) {
+    printf("Stub: pal_MutexCreate()\n");
+    *mutexHandle = (void*)0x2; // fake mutex handle
+    return 0;
 }
+
+unsigned int pal_MutexLock(KMutexHandle mutexHandle) {
+    printf("Stub: pal_MutexLock()\n");
+    return 0;
+}
+
+unsigned int pal_MutexUnlock(KMutexHandle mutexHandle) {
+    printf("Stub: pal_MutexUnlock()\n");
+    return 0;
+}
+
+void pal_MemoryFree(void* ptr) {
+    printf("Stub: pal_MemoryFree()\n");
+    // No operation
+}
+
+unsigned int pal_UtilityRandomNumber() {
+    printf("Stub: pal_UtilityRandomNumber()\n");
+    return 42; // fixed dummy value
+}
+
