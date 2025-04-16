c2c5d017e7562b9fe7e633ae8c8ee587d71ea152
shanmugapriya-93 <priya@ecrio.com>
2025-04-16 08:23:03 +0000

    final pal_stub.c with changes

--- kaios_rcs-main/lims/main/src/pal_stub.c
+++ kaios_rcs-main/lims/main/src/pal_stub.c
@@ -1,78 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
-// Stub for pal_StringNCopy to match signature in lims_wrapper.o
-unsigned int pal_StringNCopy(char* dest, const char* src, unsigned int n, unsigned int someOtherParam) {
-    printf("Stub: pal_StringNCopy('%s', '%s', %u, %u)\n", dest, src, n, someOtherParam);
-    strncpy(dest, src, n);
-    return n;
+// Stub for pal_MutexCreate
+void* pal_MutexCreate() {
+    printf("Stub: pal_MutexCreate()\n");
+    return NULL;  // Return NULL to indicate mutex creation is not supported
 }
 
-// Stub for pal_StringNConcatenate to match signature in lims_wrapper.o
-unsigned int pal_StringNConcatenate(char* dest, const char* src, unsigned int n, unsigned int someOtherParam) {
-    printf("Stub: pal_StringNConcatenate('%s', '%s', %u, %u)\n", dest, src, n, someOtherParam);
-    strncat(dest, src, n);
-    return n;
+// Stub for pal_MemoryAllocate
+void* pal_MemoryAllocate(size_t size) {
+    printf("Stub: pal_MemoryAllocate(%zu)\n", size);
+    return malloc(size);  // Allocate memory using malloc
 }
 
-// Stub for pal_UtilityRandomNumber
-unsigned int pal_UtilityRandomNumber() {
-    printf("Stub: pal_UtilityRandomNumber()\n");
-    return 12345;  // Example random number
+// Stub for pal_MemorySet
+void* pal_MemorySet(void* ptr, int value, size_t size) {
+    printf("Stub: pal_MemorySet(%p, %d, %zu)\n", ptr, value, size);
+    return memset(ptr, value, size);  // Set memory with memset
 }
 
-// Stub for pal_MutexLock
-unsigned int pal_MutexLock(void* mutexHandle) {
-    printf("Stub: pal_MutexLock()\n");
+// Stub for pal_MemoryReallocate
+void* pal_MemoryReallocate(void* ptr, size_t size) {
+    printf("Stub: pal_MemoryReallocate(%p, %zu)\n", ptr, size);
+    return realloc(ptr, size);  // Reallocate memory using realloc
+}
+
+// Stub for pal_StringFindSubString
+unsigned int pal_StringFindSubString(const char* str, const char* subStr) {
+    printf("Stub: pal_StringFindSubString('%s', '%s')\n", str, subStr);
+    return strstr(str, subStr) != NULL;  // Return 1 if found, 0 otherwise
+}
+
+// Stub for pal_StringSNPrintf
+unsigned int pal_StringSNPrintf(char* dest, unsigned int size, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    int result = vsnprintf(dest, size, format, args);
+    va_end(args);
+    return result;  // Return the result of vsnprintf
+}
+
+// Stub for pal_StringCreate
+char* pal_StringCreate(const char* str) {
+    printf("Stub: pal_StringCreate('%s')\n", str);
+    return strdup(str);  // Duplicate the string
+}
+
+// Ensure these functions match the expected signatures from lims_wrapper.o
+unsigned int pal_Deinit(int param) {
+    printf("Stub: pal_Deinit(%d)\n", param);
     return 0;  // Success
 }
 
-// Stub for pal_MutexUnlock
-unsigned int pal_MutexUnlock(void* mutexHandle) {
-    printf("Stub: pal_MutexUnlock()\n");
+unsigned int pal_Init(int param1, int param2) {
+    printf("Stub: pal_Init(%d, %d)\n", param1, param2);
     return 0;  // Success
 }
 
-// Stub for pal_MemoryFree
-void* pal_MemoryFree(void* ptr) {
-    printf("Stub: pal_MemoryFree()\n");
-    free(ptr);  // Free memory
-    return NULL;  // Return NULL to indicate memory has been freed
-}
-
-// Stub for pal_LogInit
-unsigned int pal_LogInit() {
-    printf("Stub: pal_LogInit()\n");
+unsigned int pal_LogInit(int param1, int param2, int param3, int param4, int param5, int param6) {
+    printf("Stub: pal_LogInit(%d, %d, %d, %d, %d, %d)\n", param1, param2, param3, param4, param5, param6);
     return 0;  // Success
 }
 
-// Stub for pal_Init
-unsigned int pal_Init() {
-    printf("Stub: pal_Init()\n");
-    return 0;  // Success
-}
-
-// Stub for pal_Deinit
-unsigned int pal_Deinit() {
-    printf("Stub: pal_Deinit()\n");
-    return 0;  // Success
-}
-
-// Stub for pal_SocketSetDeviceName
-unsigned int pal_SocketSetDeviceName(const char* deviceName) {
-    printf("Stub: pal_SocketSetDeviceName('%s')\n", deviceName);
-    return 0;  // Success
-}
-
-// Stub for pal_LogDeinit
-unsigned int pal_LogDeinit() {
-    printf("Stub: pal_LogDeinit()\n");
-    return 0;  // Success
-}
-
-// Stub for pal_StringLength
-unsigned int pal_StringLength(const char* str) {
-    printf("Stub: pal_StringLength('%s')\n", str);
-    return strlen(str);  // Return string length
-}
+// Other stubs...
