d654a53f0968d0aaaf3874d3851c506ca86d8e9d
shanmugapriya-93 <priya@ecrio.com>
2025-04-16 08:16:00 +0000

    new pal_stub.c

--- kaios_rcs-main/lims/main/src/pal_stub.c
+++ kaios_rcs-main/lims/main/src/pal_stub.c
@@ -1,54 +1,43 @@
 #include <stdio.h>
-#include <string.h>  // For string manipulation functions like strncpy, strncat
+#include <stdlib.h>
+#include <string.h>
 
-typedef void* KPALHandle;
-typedef void* KMutexHandle;
-typedef void* KLogHandle;
-
-unsigned int pal_Init(KLogHandle logHandle, KPALHandle* palHandle) {
-    printf("Stub: pal_Init()\n");
-    *palHandle = (void*)0x1; // fake handle
-    return 0; // success
-}
-
-void pal_Deinit(KPALHandle palHandle) {
-    printf("Stub: pal_Deinit()\n");
-}
-
-void pal_LogDeinit(KLogHandle* logHandle) {
-    printf("Stub: pal_LogDeinit()\n");
-}
-
-unsigned int pal_MutexCreate(KPALHandle palHandle, KMutexHandle* mutexHandle) {
-    printf("Stub: pal_MutexCreate()\n");
-    *mutexHandle = (void*)0x2;
-    return 0;
-}
-
-unsigned int pal_SocketSetDeviceName(KPALHandle palHandle, const char* name) {
-    printf("Stub: pal_SocketSetDeviceName('%s')\n", name);
-    return 0;
-}
-
-KLogHandle pal_LogInit(void* filename, int output, int mask, int level, int component, int type) {
-    printf("Stub: pal_LogInit()\n");
-    return (KLogHandle)0x3;
-}
-
-// Stub functions for string operations
-unsigned int pal_StringLength(const char* str) {
-    printf("Stub: pal_StringLength('%s')\n", str);
-    return (unsigned int)strlen(str);
-}
-
-unsigned int pal_StringNCopy(char* dest, const char* src, unsigned int n) {
-    printf("Stub: pal_StringNCopy('%s', '%s', %u)\n", dest, src, n);
+// Stub for pal_StringNCopy to match signature in lims_wrapper.o
+unsigned int pal_StringNCopy(char* dest, const char* src, unsigned int n, unsigned int someOtherParam) {
+    printf("Stub: pal_StringNCopy('%s', '%s', %u, %u)\n", dest, src, n, someOtherParam);
     strncpy(dest, src, n);
     return n;
 }
 
-unsigned int pal_StringNConcatenate(char* dest, const char* src, unsigned int n) {
-    printf("Stub: pal_StringNConcatenate('%s', '%s', %u)\n", dest, src, n);
+// Stub for pal_StringNConcatenate to match signature in lims_wrapper.o
+unsigned int pal_StringNConcatenate(char* dest, const char* src, unsigned int n, unsigned int someOtherParam) {
+    printf("Stub: pal_StringNConcatenate('%s', '%s', %u, %u)\n", dest, src, n, someOtherParam);
     strncat(dest, src, n);
     return n;
 }
+
+// Stub for pal_UtilityRandomNumber
+unsigned int pal_UtilityRandomNumber() {
+    printf("Stub: pal_UtilityRandomNumber()\n");
+    return 12345;  // Example random number
+}
+
+// Stub for pal_MutexLock
+unsigned int pal_MutexLock(void* mutexHandle) {
+    printf("Stub: pal_MutexLock()\n");
+    return 0;  // Success
+}
+
+// Stub for pal_MutexUnlock
+unsigned int pal_MutexUnlock(void* mutexHandle) {
+    printf("Stub: pal_MutexUnlock()\n");
+    return 0;  // Success
+}
+
+// Stub for pal_MemoryFree
+void* pal_MemoryFree(void* ptr) {
+    printf("Stub: pal_MemoryFree()\n");
+    free(ptr);  // Free memory
+    return NULL;  // Return NULL to indicate memory has been freed
+}
+
