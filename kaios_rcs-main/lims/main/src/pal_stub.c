2bb61f41f2321825317cc20dc151a71cdfbeda12
shanmugapriya-93 <priya@ecrio.com>
2025-04-17 08:58:13 +0000

    final pal_stub

--- kaios_rcs-main/lims/main/src/pal_stub.c
+++ kaios_rcs-main/lims/main/src/pal_stub.c
@@ -1,3 +1,6 @@
+#ifndef PAL_INIT_DEFINED
+#define PAL_INIT_DEFINED
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -6,14 +9,11 @@ typedef void* PALHANDLE;
 typedef void* LOGHANDLE;
 
 unsigned int pal_Init(LOGHANDLE logHandle, PALHANDLE* palInstance) {
-    if (logHandle == NULL) {
-        printf("[ERROR] Log handle is NULL\n");
-        return 1;  // Error code
-    }
     printf("[STUB] pal_Init called and returning success\n");
     *palInstance = (PALHANDLE)(uintptr_t)0x9999;  // Assign a dummy non-null pointer
     return 0;  // 0 = success
 }
+
 LOGHANDLE pal_LogInit(void* fileName, int outputType, unsigned int mask, int level, int component, int type) {
     printf("[STUB] pal_LogInit called and returning dummy handle\n");
     return (LOGHANDLE)(uintptr_t)0x8888;  // Assign dummy handle
@@ -24,3 +24,5 @@ unsigned int pal_LogDeinit(LOGHANDLE* logHandle) {
     *logHandle = NULL;
     return 0;
 }
+
+#endif
