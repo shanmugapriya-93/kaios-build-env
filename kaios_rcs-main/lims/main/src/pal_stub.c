0e9a54c9b2416dbef07bf016d35fa7de4367ee60
shanmugapriya-93 <priya@ecrio.com>
2025-04-17 08:27:18 +0000

    new pal_stub.c

--- kaios_rcs-main/lims/main/src/pal_stub.c
+++ kaios_rcs-main/lims/main/src/pal_stub.c
@@ -1,67 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
+#include <stdint.h>
 
-// Stub for pal_MutexCreate
-void* pal_MutexCreate() {
-    printf("Stub: pal_MutexCreate()\n");
-    return NULL;  // Return NULL to indicate mutex creation is not supported
+// Dummy log handle and PAL instance types
+typedef void* PALHANDLE;
+typedef void* LOGHANDLE;
+
+unsigned int pal_Init(LOGHANDLE logHandle, PALHANDLE* palInstance) {
+    printf("[STUB] pal_Init called and returning success\n");
+    *palInstance = (PALHANDLE)(uintptr_t)0x9999;  // Assign a dummy non-null pointer
+    return 0;  // 0 = success
 }
 
-// Stub for pal_MemoryAllocate
-void* pal_MemoryAllocate(size_t size) {
-    printf("Stub: pal_MemoryAllocate(%zu)\n", size);
-    return malloc(size);  // Allocate memory using malloc
+LOGHANDLE pal_LogInit(void* fileName, int outputType, unsigned int mask, int level, int component, int type) {
+    printf("[STUB] pal_LogInit called and returning dummy handle\n");
+    return (LOGHANDLE)(uintptr_t)0x8888;  // Assign dummy handle
 }
 
-// Stub for pal_MemorySet
-void* pal_MemorySet(void* ptr, int value, size_t size) {
-    printf("Stub: pal_MemorySet(%p, %d, %zu)\n", ptr, value, size);
-    return memset(ptr, value, size);  // Set memory with memset
+unsigned int pal_LogDeinit(LOGHANDLE* logHandle) {
+    printf("[STUB] pal_LogDeinit called\n");
+    *logHandle = NULL;
+    return 0;
 }
-
-// Stub for pal_MemoryReallocate
-void* pal_MemoryReallocate(void* ptr, size_t size) {
-    printf("Stub: pal_MemoryReallocate(%p, %zu)\n", ptr, size);
-    return realloc(ptr, size);  // Reallocate memory using realloc
-}
-
-// Stub for pal_StringFindSubString
-unsigned int pal_StringFindSubString(const char* str, const char* subStr) {
-    printf("Stub: pal_StringFindSubString('%s', '%s')\n", str, subStr);
-    return strstr(str, subStr) != NULL;  // Return 1 if found, 0 otherwise
-}
-
-// Stub for pal_StringSNPrintf
-unsigned int pal_StringSNPrintf(char* dest, unsigned int size, const char* format, ...) {
-    va_list args;
-    va_start(args, format);
-    int result = vsnprintf(dest, size, format, args);
-    va_end(args);
-    return result;  // Return the result of vsnprintf
-}
-
-// Stub for pal_StringCreate
-char* pal_StringCreate(const char* str) {
-    printf("Stub: pal_StringCreate('%s')\n", str);
-    return strdup(str);  // Duplicate the string
-}
-
-// Ensure these functions match the expected signatures from lims_wrapper.o
-unsigned int pal_Deinit(int param) {
-    printf("Stub: pal_Deinit(%d)\n", param);
-    return 0;  // Success
-}
-
-unsigned int pal_Init(int param1, int param2) {
-    printf("Stub: pal_Init(%d, %d)\n", param1, param2);
-    return 0;  // Success
-}
-
-unsigned int pal_LogInit(int param1, int param2, int param3, int param4, int param5, int param6) {
-    printf("Stub: pal_LogInit(%d, %d, %d, %d, %d, %d)\n", param1, param2, param3, param4, param5, param6);
-    return 0;  // Success
-}
-
-// Other stubs...
